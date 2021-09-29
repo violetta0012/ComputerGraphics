c31654e3ab89ccbaddcfb891a2f71b090ff6df58
violetta0012 <violetta0012@gmail.com>
2021-09-29 21:19:28 +0300

    try to solve 1a

--- src/main/kotlin/task1.kt
+++ src/main/kotlin/task1.kt
@@ -2,16 +2,20 @@ package my
 
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
+import javafx.scene.canvas.GraphicsContext
 import javafx.scene.control.ToggleButton
 import javafx.scene.control.ToggleGroup
+import javafx.scene.image.WritableImage
 import javafx.scene.layout.FlowPane
+import javafx.scene.paint.Color
 import javafx.scene.shape.Line
 import javafx.stage.Stage
+import java.awt.Point
 
 class Task1(primaryStage: Stage) {
     init {
         val root = FlowPane()
-        val scene = Scene(root, 800.0, 500.0)
+        val scene = Scene(root, 900.0, 500.0)
         val canvas = Canvas(600.0, 400.0)
         val context = canvas.graphicsContext2D
 
@@ -24,9 +28,12 @@ class Task1(primaryStage: Stage) {
         val group = ToggleGroup()
         val clearButton = ToggleButton("Очистить")
         val drawButton = ToggleButton("Рисовать")
+        val fillButton = ToggleButton("Заливка")
         clearButton.toggleGroup = group
         drawButton.toggleGroup = group
-        root.children.addAll(clearButton, drawButton)
+        fillButton.toggleGroup = group
+        root.children.addAll(clearButton, drawButton, fillButton)
+
         var line = Line(0.0, 0.0, 0.0, 0.0)
 
         clearButton.setOnAction {
@@ -40,10 +47,46 @@ class Task1(primaryStage: Stage) {
                 line.endY = line.startY
                 line.startX = it.sceneX
                 line.startY = it.sceneY
-                if (line.endX != 0.0 && line.endY != 0.0)
+                if (line.endX != 0.0 && line.endY != 0.0) {
+                    context.stroke = Color.RED
                     context.strokeLine(line.startX, line.startY, line.endX, line.endY)
+                }
+            }
+        }
+
+        fillButton.setOnAction {
+            root.setOnMouseClicked {
+                val image = canvas.snapshot(null, null)
+                val start = Point(it.sceneX.toInt(), it.sceneY.toInt())
+                fillWithColor(context, start, Color.AQUA, image)
             }
         }
     }
 
+    fun fillWithColor(gc: GraphicsContext, start: Point, color: Color, image: WritableImage) {
+        val pixelReader = image.pixelReader
+        val backgroundColor = pixelReader.getColor(start.x, start.y)
+        if (backgroundColor == color)
+            return
+
+        if (start.x > 0 && start.x < gc.canvas.width && start.y > 0 && start.y < gc.canvas.height &&
+                pixelReader.getColor(start.x, start.y) != color) {
+            var leftX = start.x
+            while (leftX - 1 > 0 && pixelReader.getColor(leftX - 1, start.y) == backgroundColor)
+                leftX--
+            var rightX = start.x
+            while (rightX + 1 < gc.canvas.width.toInt() && pixelReader.getColor(rightX + 1, start.y) == backgroundColor)
+                rightX++
+
+            gc.stroke = color
+            gc.strokeLine(leftX.toDouble(), start.y.toDouble(), rightX.toDouble(), start.y.toDouble());
+
+            for (x in leftX until rightX) {
+                if (start.y - 1 > 0 && pixelReader.getColor(x, start.y - 1) == backgroundColor)
+                    fillWithColor(gc, Point(x, start.y - 1), color, image)
+                if (start.y + 1 < gc.canvas.height && pixelReader.getColor(x, start.y + 1) == backgroundColor)
+                    fillWithColor(gc, Point(x, start.y + 1), color, image)
+            }
+        }
+    }
 }
